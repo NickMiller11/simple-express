8eee9153eea0a495e731da1c6e423bc0df6616b2
Nick <n@nickmiller.io>
2024-06-21 12:46:08 -0700

    test commit
    Co-authored-by: Max Hawkins <maximuskwame@gmail.com>

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = 3000;
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello Wrold");
 });
 
 app.get("/about", (req, res) => {
