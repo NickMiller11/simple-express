b9461cb80865df870076c5a99ef792797a1c8969
Nick Miller <n@nickmiller.io>
2024-10-17 14:53:37 -0700

    adding now route for demo
    Co-authored-by: Max Hawkins <maximuskwame@gmail.com>

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ const port = 3000;
 
 app.use(express.static('public'));
 
+app.get("/now", (req, res) => {
+  res.send("Giving a demo, hope it goes well")
+})
+
 app.listen(port, () => console.log('Running my sweet code!'));
